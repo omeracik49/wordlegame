3b03f0c83b532d213d354e688cf51b20174f5827
World Of Word <worldofwordapp@gmail.com>
2022-02-20 09:37:40 +0300

    wordle game

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'Material App',
+      title: 'Wordle Game',
       home: HomePage(),
       debugShowCheckedModeBanner: false,
     );
